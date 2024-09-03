b25c8d452daade36f5fce6c8dfbd61d94f7e1ac3
Diego Salazar <134554049+Kukya2@users.noreply.github.com>
2024-09-03 18:46:10 -0400

    Se añade parte 1_01

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();        
+        System.out.println(text);
+        
+}
